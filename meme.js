006688f6fae8692a1821bba32249c67c8aad2516
Aric <Aricluo415@gmail.com>
2020-09-11 17:23:11 -0700

    final commit

--- meme.js
+++ meme.js
@@ -1,13 +1,32 @@
+
+
 const myForm = document.getElementById("meme");
+
 var key = 0;
+
 myForm.addEventListener("submit", (e) => {
     e.preventDefault();
     key += 1;
-    var url = document.getElementById("url").value;
-    var topText = document.getElementById("topText").value;
-    var bottomText = document.getElementById("bottomText").value;
 
-    //Image
+    var container = createContainer(key);
+    var image = createImage(document.getElementById("url").value);
+    var topText = addText(document.getElementById("topText").value, "top");
+    var bottomText = addText(document.getElementById("bottomText").value, "bottom");
+    var remove = addRemove();
+
+    container.appendChild(image);
+    container.appendChild(topText);
+    container.appendChild(bottomText);
+    container.append(remove);
+
+    var imgs = document.getElementById("imgs");
+    imgs.appendChild(container);
+    
+    myForm.reset();
+
+});
+//Create Image DOM
+function createImage(url) {
     var img = document.createElement("img");
     img.setAttribute("class", "image");
 
@@ -15,35 +34,33 @@ myForm.addEventListener("submit", (e) => {
     img.onerror = function() {
         alert("invalid url");
     }
-    console.log("asfd");
-    var imgs = document.getElementById("imgs");
-    var k = document.createElement("div");
-    k.setAttribute("class", "container");
-    k.setAttribute("id", key);
-    imgs.appendChild(k);
-    k.appendChild(img);
-    //Top Text
-    var tdiv = document.createElement("div");
-    tdiv.setAttribute("class", "top");
-    var ttext = document.createTextNode(topText);
-    tdiv.appendChild(ttext);
-    k.appendChild(tdiv);
-    //Bottom Text
-    var bdiv = document.createElement("div");
-    bdiv.setAttribute("class", "bottom");
-    var btext = document.createTextNode(bottomText);
-    bdiv.appendChild(btext);
-    k.appendChild(bdiv);
-    //Remove button
+
+    return img;
+}
+//Create Container DOM 
+function createContainer(key) {
+    var container = document.createElement("div");
+    container.setAttribute("class", "container");
+    container.setAttribute("id", key);
+    return container;
+}
+//Add Text to container
+function addText(text, textPos) {
+    var div = document.createElement("div");
+    div.setAttribute("class", textPos);
+    var textNode = document.createTextNode(text);
+    div.appendChild(textNode);
+    return div;
+}
+//add a remove button
+function addRemove() {
     var remove = document.createElement("button");
     remove.setAttribute("class", "btn");
-    var txt = document.createTextNode("X");
-    remove.appendChild(txt);
+    var x = document.createTextNode("X");
+    remove.appendChild(x);
     remove.addEventListener("click", (e) => {
         var element = document.getElementById(e.target.parentNode.id);
         element.parentNode.removeChild(element);
     })
-    k.appendChild(remove);
-    myForm.reset();
-
-});
+    return remove;
+}
